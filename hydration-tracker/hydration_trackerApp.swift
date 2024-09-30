5fc272723c4265122f2962b7bd16156da1146b92
Celena Valenzuela <celenaisabelle@outlook.com>
2024-09-30 11:46:28 -0400

    First commit.

---
+++ hydration-tracker/hydration_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydration_trackerApp.swift
+//  hydration-tracker
+//
+//  Created by Celena Valenzuela on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct hydration_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ hydration-trackerTests/hydration_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  hydration_trackerTests.swift
+//  hydration-trackerTests
+//
+//  Created by Celena Valenzuela on 9/30/24.
+//
+
+import Testing
+@testable import hydration_tracker
+
+struct hydration_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
